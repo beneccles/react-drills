728c6e93c65924ef8935480aee2a6313477a02cd
Ben Eccles <ben.g.eccles@icloud.com>
2019-08-27 15:49:58 -0600

    added solution to problem 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      filter: '',
+      values: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese']
+    }
+  }
+
+  handleChange(value){
+    this.setState({filter: value})
+  }
+
   render() {
+    // Filter the values in this, and see if they have the characters from the input.
+    let newArr = this.state.values.filter((el, i) => {
+      return el.includes(this.state.filter);
+    })
+
+    // Map the new array to display, with the needed H2 tags.
+    let displayArr = newArr.map((el, index) => {
+      return <h2 key={index}>{el}</h2>
+    })
+
     return (
+
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={e => this.handleChange(e.target.value)} type="text" /> 
+        {/* On change of text box, update this.state.filter to user input and display filtered list*/}
+          {displayArr}
+          {console.log(displayArr)}
       </div>
     );
   }
